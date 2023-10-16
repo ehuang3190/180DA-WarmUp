3076d69d50c0cd158e3be83b5af327df56a7eae2
Enoch Huang <eihuang@g.ucla.edu>
2023-10-16 08:06:31 +0100

    Update test.py to make more flexible

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     x = input(f"Which course are you taking?")
-    if x == "ECE 180":
+    if x == "ECE 180" or x == "ECE 180DA":
         print(x + " is the best class ever")
     else:
         print("Bruins are the best!")
